28ba324763828f4aec84d0bb071d05b5fa06e5d0
TakumiSei <d21115@numazu.kosen-ac.jp>
2024-09-15 15:23:17 +0900

    arduino_run_syuusei

--- program/arduino/main/define.h
+++ program/arduino/main/define.h
@@ -40,6 +40,6 @@ int   pwm_limitter_H  = 80;
 int   pwm_limitter_L  = -80;
 int   motor_l_offset  = 0;
 int   motor_r_offset  = 0;
-int   dead_zone       = 30;
+int   dead_zone       = 40;
 
 int   T_GIVE          = 100;  //[sec]
