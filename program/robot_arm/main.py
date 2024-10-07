fdc90d4276c6556b6bd8b489d594909a0414da47
marochoco <marochoco.kuron@gmail.com>
2024-10-07 15:07:45 +0900

    追加

--- program/robot_arm/main.py
+++ program/robot_arm/main.py
@@ -3,36 +3,38 @@
 import math
 import numpy as np
 import time
-
-serial()    #jetsonからエンドエフェクタ取得
-
-L = [1,1,1,1,1]     #関節長さ
-start_theta = [0,0,0,0,0]
-current_theta = []
-goal_theta = []
-end_effecter = [x,y,z]
-lambda = np.array[1,1,1]
-
-qx = lambda[1]*math.sin(theta)
-qy = lambda[2]*math.sin(theta)
-qz = lambda[3]*math.sin(theta)
-qw = math.cos(theta)
-quaternion  = [qx,qy,qz,qw]
-
-position_4 = end_effecter-(L[4]+L[5])*
+import config
+from serial_mas_ras import init_serial, send_matrix, close_serial
+from serial_sla_ras import
 
 
+def main():
+    end_effecter = send_data_jr()    #シリアル通信（jetson to raspberrry pi）の関数呼び出し
+    lam = np.array[1,1,1,]
 
 
-#数値計算
+    qx = lam[1]*math.sin(theta)
+    qy = lam[2]*math.sin(theta)
+    qz = lam[3]*math.sin(theta)
+    qw = math.cos(theta)
+    quaternion  = [qx,qy,qz,qw]
+    position_4 = end_effecter-(L[4]+L[5])*
 
+    #数値計算
+    x=l1*math.cos(theta1)+l2*math.cos(theta1+theta2)+
+    y=l1*math.sin(theta1)+l2*math.sin(theta1+theta2)+
 
+    # シリアル通信の初期化
+    ser = init_serial()
 
-x=l1*math.cos(theta1)+l2*math.cos(theta1+theta2)+
-y=l1*math.sin(theta1)+l2*math.sin(theta1+theta2)+
+    # 各間接角度
+    matrix = np.array([[1, 2], [3, 4]])
+    
+    # 行列をピコに送信
+    send_matrix(ser, matrix)
 
+    # シリアルポートをクローズ
+    close_serial(ser)
 
-
-
-
-
+    if __name__ == "__main__":
+        main()
---
+++ program/serial/serial_mas_jetson.py
@@ -0,0 +1,6 @@
+#jetson nano → raspberry pi シリアル通信（jetson nano 側）
+
+import serial
+
+
+
