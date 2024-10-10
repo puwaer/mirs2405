5a27abc9afdf84c97a27ef97b9102f7c62288667
TakumiSei <d21115@numazu.kosen-ac.jp>
2024-10-10 18:57:20 +0900

    IO修正

--- program/IO/IO.ino
+++ program/IO/IO.ino
@@ -6,6 +6,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  check_raspy();
 }
---
+++ program/IO/raspi.ino
@@ -0,0 +1,38 @@
+void check_raspy(){
+  int inc_bytes[64] = {-1,};
+
+    /*ここから受信処理*/
+    if(Serial.available() > 0){
+        if(Serial.read() == 255){
+            int sread = 0;
+            int idx = 0;
+            while(1){
+                while(Serial.available() < 1){}
+                sread = (int)Serial.read();
+                if(sread == 254){
+                    break;
+                }else if(sread != -1){
+                    inc_bytes[idx] = sread;
+                    idx += 1;
+                }
+            }
+        }
+    }
+    /*ここまで*/
+
+
+    /*ここから受信データに応じた処理*/
+    if(inc_bytes[0] == -1){
+        //なんもなければ終了
+        return;
+    }
+    switch(inc_bytes[0]){
+        case 1:
+            
+            break;
+        default:
+            break;
+    }
+
+
+}
