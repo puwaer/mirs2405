3498633c79791c557b08f211ae4de414a793ce3f
marochoco <marochoco.kuron@gmail.com>
2024-10-18 16:10:25 +0900

    a

---
+++ program/serial/serial_jetson.py
@@ -0,0 +1,25 @@
+#jetson nano → raspberry pi シリアル通信（jetson nano 側）
+
+import serial
+import numpy as np
+import struct
+
+def init_serial(port='/dev/ttyUSB0', baudrate=9600):
+    """シリアル通信の初期化"""
+    ser = serial.Serial(port, baudrate, timeout=1)
+    return ser
+
+def send_xyz(ser, position):
+    """x, y, z座標をシリアル送信"""
+    ser.write(struct.pack('fff', *position))
+
+def close_serial(ser):
+    """シリアル通信を閉じる"""
+    ser.close()
+
+
+#mainでの使用
+position = np.array[0, 0, 0]
+ser = init_serial()
+send_xyz(ser, position)
+close_serial()
