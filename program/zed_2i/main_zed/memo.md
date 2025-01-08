aa3ff1cc0446197f824b76324f67a156d35e566f
kakeru1126 <kakel20151126@gmail.com>
2025-01-09 07:21:19 +0900

    zed

---
+++ program/zed_2i/main_zed/memo.md
@@ -0,0 +1,11 @@
+サーバー
+main_height_server.py
+server_value_updata.py
+zed_hight_class.py
+
+
+クライアント
+client_json.py
+
+jsonファイルread
+read_json.py
---
+++ program/zed_2i/main_zed/read_json.py
@@ -0,0 +1,20 @@
+import json
+
+def read_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+def read_json_name(file_number):
+    file_path = [
+        './program/output/output_age.json',
+        './program/output/output_gender.json',
+        './program/output/output_zed.json'
+        ]
+    
+    return int(read_json(file_path[file_number]))
+    
+
+if __name__ == "__main__":
+    json_data = read_json_name(2)       #[0, 1, 2],[男女, 年齢, 身長]
+    print(json_data)
