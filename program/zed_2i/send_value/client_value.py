d5b31c599f0107379a7920f2b94fdbdc5d5e596b
puwaer <y_50uki@outlook.jp>
2025-01-09 01:14:42 +0900

    client

--- program/zed_2i/send_value/client_value.py
+++ program/zed_2i/send_value/client_value.py
@@ -1,7 +1,7 @@
 import socket
 import json
 
-HOST = "172.25.16.100"  
+HOST = "172.25.15.27"  
 PORT = 5700
 
 def get_dict_data(sock):
@@ -17,19 +17,20 @@ def get_dict_data(sock):
     json_str = buf.decode('utf-8')
     return json.loads(json_str)
 
-# ソケット接続
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
+if __name__ == "__main__":
+    # ソケット接続
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
 
-try:
-    while True:
-        # データを受信して表示
-        received_data = get_dict_data(sock)
-        print(received_data)  # または必要な処理を実行
-        
-except KeyboardInterrupt:
-    print("\nClosing connection...")
-except Exception as e:
-    print(f"Error occurred: {e}")
-finally:
-    sock.close()
+    try:
+        while True:
+            # データを受信して表示
+            received_data = get_dict_data(sock)
+            print(received_data)  # または必要な処理を実行
+            
+    except KeyboardInterrupt:
+        print("\nClosing connection...")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        sock.close()
