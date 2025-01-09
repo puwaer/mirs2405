05e63e40a0b3584fba02b4d337a463a40e7da0a5
puwaer <y_50uki@outlook.jp>
2025-01-09 13:18:12 +0900

    rename

--- program/zed_2i/send_value/client_value_updata.py
+++ program/zed_2i/send_value/client_value_updata.py
@@ -1,26 +1,19 @@
 import socket
 import json
+import re
 
 def get_dict_data(sock):
-    #data = sock.recv(1024).decode('utf-8')  # 受信データをデコード
+    data = sock.recv(1024).decode('utf-8')  # 受信データをデコード
     #print(data)  # 受信データを表示
-    # 最初の4バイトを受信してサイズ情報を取得
-    size_data = sock.recv(4)
-    if len(size_data) < 4:
-        raise ValueError("Failed to receive size information")
-    size = int.from_bytes(size_data, byteorder='big')
-
-    # サイズに基づいてデータを受信
-    data = sock.recv(size).decode('utf-8')
     
-    return data
+    # 制御文字や不要な部分を除去
+    clean_data = re.sub(r'[^\d]', '', data)  # 数字以外を削除
+    return clean_data
 
 if __name__ == "__main__":
-    #HOST = '172.25.15.27'
-    HOST = '172.25.15.130'
+    HOST = '172.25.15.27'
     PORT = 5700
     while True:
-        received_data_int = 0
         try:
             # ソケット接続
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,13 +21,13 @@ if __name__ == "__main__":
 
             # データを受信して表示
             received_data = get_dict_data(sock)
-            received_data_int = int(json.loads(received_data))
-            if (received_data_int != 0):
-                print(received_data_int)
-                received_data_int = 0
-                break
+            int_data = int(received_data)
+            print(int_data)
+            
+            
             # 接続を閉じる
             sock.close()
+            
         except KeyboardInterrupt:
             print("\nClosing connection...")
             break
@@ -42,4 +35,3 @@ if __name__ == "__main__":
             print(f"Error occurred: {e}")
             sock.close()
             continue
-        
